fb65a4a9918bf46445d6951574d8b67edfb2ddb1
kasicass <kasicass@gmail.com>
2021-03-22 15:12:53 +0800

    update shaders

--- Code/Samples/01-UseDxc/compile.cmd
+++ Code/Samples/01-UseDxc/compile.cmd
@@ -1,3 +1,3 @@
 set DXC="..\..\External\dxc\bin\x64\dxc.exe"
-%DXC% -T vs_6_0 -E main -Fo simple.dxil /Zi simple.hlsl
-rem %DXC% -T vs_6_0 -E FpsVS -Fo simple.dxil fps.hlsl
+%DXC% -T vs_6_0 -E main -Fo SimpleVS.dxil SimpleVS.hlsl
+%DXC% -T ps_6_0 -E main -Fo SimplePS.dxil SimplePS.hlsl
